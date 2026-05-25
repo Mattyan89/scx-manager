73b805356bfc974f9e5aa0f28528fcfac97546fc
Julio Napurí <julionc@gmail.com>
2026-05-25 17:13:45 -0500

    i18n: add Spanish translation (#12)

---
+++ lang/scx-manager_es.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>SchedExtWindow</name>
+    <message>
+        <location filename="../src/schedext-window.ui" line="17"/>
+        <source>CachyOS Configure sched-ext</source>
+        <translation>Configuración de sched-ext de CachyOS</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="40"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Configure sched-ext scheduler:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Configurar planificador sched-ext:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="75"/>
+        <source>Running sched-ext scheduler:</source>
+        <translation>Planificador sched-ext en ejecución:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="82"/>
+        <source>Select sched-ext scheduler:</source>
+        <translation>Seleccionar planificador sched-ext:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="92"/>
+        <source>Select scheduler profile:</source>
+        <translation>Seleccionar perfil del planificador:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="115"/>
+        <source>unknown</source>
+        <translation>desconocido</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="122"/>
+        <source>Set sched-ext extra scheduler flags:</source>
+        <translation>Establecer flags adicionales del planificador sched-ext:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="176"/>
+        <source>Disable</source>
+        <translation>Deshabilitar</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="183"/>
+        <source>Apply</source>
+        <translation>Aplicar</translation>
+    </message>
+</context>
+<context>
+    <name>scxctl::impl::SchedExtWindow</name>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="122"/>
+        <source>Cannot initialize scx_loader configuration</source>
+        <translation>Error al inicializar la configuración de scx_loader</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="138"/>
+        <source>Cannot get information from scx_loader!
+Is it working?
+This is needed for the app to work properly</source>
+        <translation>¡Error al obtener información de scx_loader!
+¿Está en funcionamiento?
+Esto es necesario para que la aplicación funcione correctamente</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="205"/>
+        <source>Cannot disable scx_loader</source>
+        <translation>Error al deshabilitar scx_loader</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="223"/>
+        <source>Cannot get scx flags from scx_loader configuration!</source>
+        <translation>¡Error al obtener los flags de scx desde la configuración de scx_loader!</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="259"/>
+        <source>Cannot set default scx scheduler with mode! Scheduler %1 with mode %2</source>
+        <translation>¡Error al establecer el planificador scx por defecto! Planificador %1 con modo %2</translation>
+    </message>
+</context>
+</TS>
