f30fea3f5921c08cc378d62a3b648f4c01f15b0a
Mattyan89 <cimarelli.matteo@gmail.com>
2026-06-11 19:38:14 +0200

    Update Italian translations in scx-manager_it.ts

---
+++ lang/scx-manager_it.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>SchedExtWindow</name>
+    <message>
+        <location filename="../src/schedext-window.ui" line="17"/>
+        <source>CachyOS Configure sched-ext</source>
+        <translation>Configuratore sched-ext di CachyOS</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="40"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Configure sched-ext scheduler:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Configura lo scheduler sched-ext:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="75"/>
+        <source>Running sched-ext scheduler:</source>
+        <translation>Scheduler sched-ext in esecuzione:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="82"/>
+        <source>Select sched-ext scheduler:</source>
+        <translation>Seleziona lo scheduler sched-ext:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="92"/>
+        <source>Select scheduler profile:</source>
+        <translation>Seleziona il profilo dello scheduler:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="115"/>
+        <source>unknown</source>
+        <translation>sconosciuto</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="122"/>
+        <source>Set sched-ext extra scheduler flags:</source>
+        <translation>Imposta i flag aggiuntivi dello scheduler sched-ext:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="176"/>
+        <source>Disable</source>
+        <translation>Disabilita</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="183"/>
+        <source>Apply</source>
+        <translation>Applica</translation>
+    </message>
+</context>
+<context>
+    <name>scxctl::impl::SchedExtWindow</name>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="122"/>
+        <source>Cannot initialize scx_loader configuration</source>
+        <translation>Impossibile inizializzare la configurazione di scx_loader</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="138"/>
+        <source>Cannot get information from scx_loader!
+Is it working?
+This is needed for the app to work properly</source>
+        <translation>Impossibile ottenere informazioni da scx_loader!
+È in funzione?
+Questo è necessario affinchè l&apos;applicazione funzioni correttamente</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="205"/>
+        <source>Cannot disable scx_loader</source>
+        <translation>Impossibile disabilitare scx_loader</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="223"/>
+        <source>Cannot get scx flags from scx_loader configuration!</source>
+        <translation>Impossibile ottenere i flag dalla configurazione di scx_loader!</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="259"/>
+        <source>Cannot set default scx scheduler with mode! Scheduler %1 with mode %2</source>
+        <translation>Impossibile impostare lo scheduler scx predefinito nella modalità selezionata! Scheduler %1 in modalità %2</translation>
+    </message>
+</context>
+</TS>
